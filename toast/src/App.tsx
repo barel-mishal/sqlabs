92305a63658ef53908d715738e0c90a61807c0a5
barel-mishal <barel.mishal@mail.huji.ac.il>
2023-11-21 19:33:06 +0200

    counter

--- toast/src/App.tsx
+++ toast/src/App.tsx
@@ -1,157 +1,39 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css';
-import Container from 'react-bootstrap/esm/Container';
-import CenteredButton from './components/PokeProject/CenteredButton';
-import { Card as PokemonCard } from 'pokemon-tcg-sdk-typescript/dist/sdk';
-import './pokemonCard.css'
-import Card from 'react-bootstrap/esm/Card';
-import ListGroup from 'react-bootstrap/esm/ListGroup';
 
-// Enum for application events
-enum AppEvents {
-  Initialize,
-  FetchPokemons,
-  DisplayPokemon,
-  DisplayError,
-  DisplayLoading,
-}
 
-interface FetchResponse {
-  data: PokemonCard[];
-  page: 1;
-  pageSize: 250;
-  count: 250;
-  totalCount: 17174;
-}
 
 function App() {
-  // State to manage Pokémons, loading status, and errors
-  const [pokemons, setPokemons] = useState<PokemonCard[]>([]);
-  const [appEvent, setAppEvent] = useState<AppEvents>(AppEvents.Initialize);
-  const [errorMsg, setErrorMsg] = useState('');
-  useEffect(() => {
-    const local = localStorage.getItem('pokemons');
-    if (local !== null) {
-      setPokemons(JSON.parse(local));
-      setAppEvent(AppEvents.DisplayPokemon);
-      return;
-    }
-  }, [pokemons]);
-  // Function to fetch pokemons
-  const fetchPokemon = async () => {
-    const local = localStorage.getItem('pokemons');
-    if (local !== null) {
-      setPokemons(JSON.parse(local));
-      setAppEvent(AppEvents.DisplayPokemon);
-      return;
-    }
-    try {
-      setAppEvent(AppEvents.DisplayLoading);
-      setErrorMsg('');  
-      const response = await fetch('https://api.pokemontcg.io/v2/cards');
-
-      if (response.ok) {
-        setAppEvent(AppEvents.FetchPokemons);
-        const data: FetchResponse = await response.json();
-        setPokemons(data.data);
-        setAppEvent(AppEvents.DisplayPokemon);
-
-        localStorage.setItem('pokemons', JSON.stringify(data.data));
-      } else {
-        // Handle HTTP errors
-        setAppEvent(AppEvents.DisplayError);
-        setErrorMsg(`Failed to fetch Pokémon data: ${response.status} ${response.statusText}`);
-      }
-    } catch (err) {
-      // Handle network errors or issues with fetch
-      setAppEvent(AppEvents.DisplayError);
-      setErrorMsg('Network error occurred while fetching Pokémon data');
-    } finally {
-      setAppEvent(AppEvents.DisplayPokemon);
-    }
-  };
 
   return (
     <>
-        {appEvent === AppEvents.Initialize 
-        ? <Container className='h-screen items-center flex justify-content'>
-          <CenteredButton onClick={fetchPokemon}>Fetch Pokemon</CenteredButton>
-        </Container> : 
-        appEvent === AppEvents.DisplayLoading
-        ? <p>Loading...</p> : 
-        appEvent === AppEvents.DisplayPokemon
-        ? <Pokemons pokemons={pokemons}/> :
-        appEvent === AppEvents.DisplayError
-        ? <p>Error: {errorMsg}</p> : <p>Unknown error occurred</p>}
+        <Counter />
     </>
   );
 }
 
 export default App;
 
-export const Pokemons = (props: {pokemons: PokemonCard[]}) => {
-  const [showImg, setShowImg] = useState(true);
-
-  return <div>
-    <div><button onClick={() => setShowImg(() => !showImg)}>Show Image Card</button></div>
-    <div className='pokemons'>
-    {
-      props.pokemons.map(pokemon => {
-        return <><Card style={{ }}>
-              <Card.Img hidden={showImg} variant="top" src={pokemon.images.small} />
-              <Card.Body>
-                <Card.Title style={{display: 'flex', justifyContent: 'space-between'}}><span>{pokemon.name}</span><span>{pokemon.hp}<span>hp</span></span></Card.Title>
-                {pokemon.flavorText && <Card.Text>
-                  {pokemon.flavorText}
-                </Card.Text>}
-              </Card.Body>
-              <ListGroup className="list-group-flush">
-                {pokemon.abilities && <ListGroup.Item style={{display: 'flex', gap: 4}}>{pokemon.abilities?.map(ability => <span key={ability.name}>{ability.name}</span>)}</ListGroup.Item>}
-                {pokemon.attacks && <ListGroup.Item style={{display: 'flex', gap: 4}}>{pokemon.attacks?.map(attack => <span key={attack.name}>{attack.name}</span>)}</ListGroup.Item>}
-                <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-              </ListGroup>
-              <Card.Body>
-                <div className='pokemon-basic-details'>
-                  <p>{pokemon.id}</p>
-                  <p>{pokemon.supertype}</p>
-                  <p>{pokemon.subtypes.join(', ')}</p>
-                </div>
-
-                <div className='pokemon-attributes'>
-                  <p>{pokemon.hp}</p>
-                  <p>{pokemon?.types?.join(', ')}</p>
-                </div>
-
-                <div className='pokemon-evolution-details'>
-                  <p>{pokemon.evolvesFrom}</p>
-                  <p>{pokemon.evolvesTo?.join(', ')}</p>
-                </div>
-
-                <div className='pokemon-abilities-attacks'>
-                  <p>{pokemon.abilities?.map(ability => <span key={ability.name}>{ability.name}</span>)}</p>
-                  <p>{pokemon.attacks?.map(attack => <span key={attack.name}>{attack.name}</span>)}</p>
-                </div>
-
-                <div className='pokemon-set-information'>
-                  <p>{pokemon.set.name}</p>
-                  <p>{pokemon.set.releaseDate}</p>
-                </div>
-
-                <div className='pokemon-additional-info'>
-                  <p>{pokemon.artist}</p>
-                  <p>{pokemon.rarity}</p>
-                </div>
-
-                <div className='pokemon-legalities-pricing'>
-                  {Object.entries(pokemon.legalities).map(([key, value]) => <p><span key={key}>{`${key}: ${value}`}</span></p>)}
-                </div>
-              </Card.Body>
-            </Card>
-     
-          </>
-    })
+export const Counter = () => {
+  const [count, setCount] = useState(0);
+  const increment = () => setCount(() => count+1);
+  const decrement = () => setCount(() => count-1);
+  useEffect(() => {
+    // max 20 min 0
+    if(count > 20) {
+      setCount(20)
     }
-  </div></div>
+    if (count < 0) {
+      setCount(0)
+    }
+  }, [count])
+  return <div className='counter'>
+    <div className='screen-of-counter big'><h2>{count}</h2></div>
+    <button className='red' disabled={count <= 0} onClick={decrement}>-1</button>
+    <button className='green' disabled={count >= 20} onClick={increment}>+1</button>
+    <div className='screen-of-counter small'><p>Counter: {count}</p></div>
+  </div>
 }
+
